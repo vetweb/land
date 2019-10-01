e34712872b392a5ae9dca1d94ffce63be9d66857
idalgo <vetweba@gmail.com>
2019-10-01 09:27:35 +0300

    Landing Infra. First step;

---
+++ src/routes/index.svelte
@@ -0,0 +1,57 @@
+<script>
+	const mode = process.env.NODE_ENV || 'development'
+</script>
+
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+	<img alt='Borat' src='/great-success.png'>
+	<figcaption>HIGH FIVE!</figcaption>
+	<a href="/test/">ссыл</a>
+
+</figure>
+
+<p>
+	<span>{mode}</span>
+	<br>
+	<strong>
+
+	Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
